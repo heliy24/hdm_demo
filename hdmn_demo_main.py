1543cb0588d2a487960d4be2d0cfaca9a961e455
heliy24 <heliy24@mail.ru>
2025-10-23 16:30:58 +0300

    Update hdmn_demo_main.py
    change web-site

--- hdmn_demo_main.py
+++ hdmn_demo_main.py
@@ -3,13 +3,14 @@ import requests
 #url = 'https://hidemyname.org/ru/demo/' #to 30.08.2025
 #url = 'https://hxdemy.name/demo/'# from 30.08.2025 to 25.09.2025
 #url = 'https://hide-my-name.org/demo/' # from 25.09.2025
-url = 'https://hide-my-name.com/demo/' # from 06.10.2025
+#url = 'https://hide-my-name.com/demo/' # from 06.10.2025
+url = 'https://hide-my-name.org/demo/' # from 23.10.2025
 
 if 'Ваша электронная почта' in requests.get(url).text:
     
     email = input('Введите электронную почту для получения тестового периода: ')
 
-    response = requests.post('https://hide-my-name.com/demo/success/', data={
+    response = requests.post('https://hide-my-name.org/demo/success/', data={
         "demo_mail": f"{email}"
     })
 

f611201754bd320cf7cec637bfc4a6032cdb9be3
heliy24 <heliy24@mail.ru>
2026-06-22 15:14:21 +0300

    Update hdmn_demo_main.py

--- hdmn_demo_main.py
+++ hdmn_demo_main.py
@@ -8,6 +8,7 @@ import requests
 #url = 'https://hide-my-name.com/' # from 01.02.2026
 #url = 'https://safeclick.email/' # from 01.02.2026
 url = 'https://hide-my-name.app/' # from 26.02.2026
+url = 'https://hide-my-name.cc/' # from 22.06.2026
 
 if 'Ваша электронная почта' in requests.get(url).text:
     
